61e9a98b6865160603054fc1b56a4e73f1e3348d
wagner caetano <wagnercaetano01@gmail.com>
2020-10-13 22:20:10 -0300

    update

---
+++ content.js
@@ -0,0 +1,40 @@
+(function () {
+  window.addEventListener('load', afterWindowLoaded);
+
+  function afterWindowLoaded() {
+    let btnCon = document.querySelector('#paywall-upsell-button-upgrade');
+    if (!btnCon) return;
+
+    let btnReadFree = btnCon.children[0].cloneNode();
+    btnReadFree.id = 'btnReadForFree';
+    btnReadFree.classList.remove('nf');
+    btnReadFree.classList.remove('nd');
+    btnReadFree.style.backgroundColor = '#319ce8';
+    btnReadFree.style.borderColor = '#319ce8';
+    btnReadFree.style.marginLeft = '3px';
+    btnReadFree.style.width = 'fit-content';
+    btnReadFree.href = '#';
+    btnReadFree.text = 'Read for free';
+
+    btnCon.appendChild(btnReadFree);
+
+    document.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      if (e.target && e.target.id == 'btnReadForFree') {
+        btnReadFree.text = 'Page content loading ...';
+        replacePageContent();
+      }
+    });
+  }
+
+  function replacePageContent() {
+    fetch(window.location.href, {
+      method: 'GET',
+      credentials: 'omit',
+      mode: 'no-cors',
+    })
+      .then((response) => response.text())
+      .then((content) => document.write(content));
+  }
+})();
